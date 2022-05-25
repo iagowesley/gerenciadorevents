f1cd26dd8193bcc9f19af347d8703ac3b013701f
iago.costa <iago.costa@fitbank.com.br>
2022-05-25 15:33:08 -0300

    alter

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import styles from './Home.module.css'
 import carraca from '../../img/carraca.svg'
+import { Link } from 'react-router-dom'
+import LinkButton from '../layout/LinkButton'
 
 function Home() {
     return (
         <section className= {styles.home_container}>
             <h1>Bem vindo ao <span>Events</span></h1>
             <p>Comece a gerenciar seus eventos agora mesmo!</p>
-            <a href = "/">Criar Projeto</a>
+            <LinkButton to ="/newproject" text="Criar projeto"/> 
             <img src={carraca} alt = "Events"/>
         </section>
     )
